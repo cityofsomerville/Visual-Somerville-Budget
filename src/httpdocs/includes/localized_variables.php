2c310f5bbbc3f7fc1a4560959e4378cce64a11c1
mmastrobuoni <michaelmastrobuoni@gmail.com>
2016-06-06 12:09:27 -0400

    Change Town to City
    Changes a few instances of the word Town to City. Also changed the
    website url so it works

--- src/httpdocs/includes/localized_variables.php
+++ src/httpdocs/includes/localized_variables.php
@@ -1,8 +1,8 @@
 <?php
   # Variables used through the site
   $siteName = "Somerville Visual Budget";
-  $siteURL = "somervillema.gov";
-  $municipalURL = "http://somervillema.gov";
+  $siteURL = "www.somervillema.gov";
+  $municipalURL = "http://www.somervillema.gov";
   $feedbackEmail = "somerstat@somervillema.gov";
   $shortName = "Somerville";
   $longName = "City of Somerville";
